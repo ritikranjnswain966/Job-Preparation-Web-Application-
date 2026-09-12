6bae34ef653bfceed8aa13c238742ec5ed1c4a6a
Ritik Ranjn Swain <ritikranjnswain966@gamil.com>
2026-09-12 12:06:17 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./src/app');
 const connectToDB = require("./src/config/database")
 
-await connectToDB()
+connectToDB()
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
