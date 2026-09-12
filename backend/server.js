812fba44ed3624ef157fd497e4b4b285b35b25c2
Ritik Ranjn Swain <ritikranjnswain966@gamil.com>
2026-09-12 17:19:17 +0530

    ..

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const connectToDB = require("./src/config/database")
 
 connectToDB()
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+if (!process.env.VERCEL) {
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+}
+
+module.exports = app
